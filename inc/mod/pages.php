01feb137a64112cca7a95b003147494ba8f9a2fa
ArisuAeryi <31854580+ArisuAeryi@users.noreply.github.com>
2017-09-13 23:58:33 +1000

    Fix moving threads with deleted images (#2)
    * Fix moving threads with deleted images
    
    This works currently on my hardware. I can't think of any edge cases that would break this yet
    
    * More error handling

--- inc/mod/pages.php
+++ inc/mod/pages.php
@@ -1355,8 +1355,10 @@ function mod_move($originBoard, $postID) {
 			if ($post['has_file']) {
 				// copy image
 				foreach ($post['files'] as $i => &$file) {
-					$clone($file['file_path'], sprintf($config['board_path'], $board['uri']) . $config['dir']['img'] . $file['file']);
-					$clone($file['thumb_path'], sprintf($config['board_path'], $board['uri']) . $config['dir']['thumb'] . $file['thumb']);
+					if ($file['file'] !== 'deleted') //if file exists, process it
+						$clone($file['file_path'], sprintf($config['board_path'], $board['uri']) . $config['dir']['img'] . $file['file']);
+					if (isset($file['thumb']) && !in_array($file['thumb'], array('spoiler', 'deleted', 'file'))) //if thumb is not spceial, process it's file
+						$clone($file['thumb_path'], sprintf($config['board_path'], $board['uri']) . $config['dir']['thumb'] . $file['thumb']);
 				}
 			}
 			// insert reply
