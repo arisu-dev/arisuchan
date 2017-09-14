7d97fc3a55e0256315b4658a4aa3f663bd82d4d9
Composition Fore <composition4project@gmail.com>
2017-09-14 16:14:20 -0400

    fix #55 - remove aditional file upload fields

--- js/multi-image.js
+++ js/multi-image.js
@@ -10,23 +10,46 @@
  */
 
 function multi_image() {
-    $('input[type=file]').after('<a href="#" class="add_image">+</a>');
+    // $('input[type=file]').after('<a href="#" class="add_image">+</a>');
+
+    // $('input[type^=file]').hide();
+    // $('*[data-ac-filenum]').hide();
+    // $('*[data-ac-filenum==1]').show();
     
-    $(document).on('click', 'a.add_image', function(e) {
-        e.preventDefault();
+    $('a.file_add').click(function(ev) {
+        var fileNum = $(ev.currentTarget).parent().data('ac-filenum');
+        var images_len = $('*[data-ac-filenum]').length;
+        if (fileNum != images_len)
+            $('*[data-ac-filenum='+(fileNum+1)+']').show();
+        ev.preventDefault();
+    });
 
-        var images_len = $('form:not([id="quick-reply"]) [type=file]').length;
-        
-        if (!(images_len >= max_images)) {
-            var new_file = '<br class="file_separator"/><input type="file" name="file'+(images_len+1)+'" id="upload_file'+(images_len+1)+'">';
+    $('a.file_rm').click(function(ev) {
+        var fileNum = $(ev.currentTarget).parent().data('ac-filenum');
+        if (fileNum != 1)
+            $('*[data-ac-filenum='+(fileNum-1)+']').hide();
+        console.debug("Hiding: ", ev.currentTarget, fileNum-1);
+        ev.preventDefault();
+    });
 
-            $('[type=file]:last').after(new_file);
-            if ($("#quick-reply").length) {
-                $('form:not(#quick-reply) [type=file]:last').after(new_file);
-            }
-            if (typeof setup_form !== 'undefined') setup_form($('form[name="post"]'));
-        }
-    })
+    // $(document).on('click', 'a.add_image', function(e) {
+    //     e.preventDefault();
+    //
+    //     var images_len = $('form:not([id="quick-reply"]) [type=file]').length;
+    //
+    //     if (!(images_len >= max_images)) {
+    //         var new_file = '<br class="file_separator"/>' +
+    //                        '<input type="file" name="file' + (images_len+1) + '" id="upload_file' + (images_len+1) + '">' +
+    //                        '<a class="add_image" data-file="file' + (images_len+1)
+    //                        '<a class="add_image" data-file="file' + (images_len+2) + '"> - </a>';
+    //
+    //         $('[type=file]:last').after(new_file);
+    //         if ($("#quick-reply").length) {
+    //             $('form:not(#quick-reply) [type=file]:last').after(new_file);
+    //         }
+    //         if (typeof setup_form !== 'undefined') setup_form($('form[name="post"]'));
+    //     }
+    // })
 }
 
 if (active_page == 'thread' || active_page == 'index' && max_images > 1) {
